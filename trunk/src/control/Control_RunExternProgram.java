c3caa2256133052bedd494f184efd372c8521521
eule <eule@163ef220-f67a-0410-9be0-c85765f07cbc>
2009-12-18 16:16:05 +0000

    use packages now

---
+++ trunk/src/control/Control_RunExternProgram.java
@@ -0,0 +1,21 @@
+package control;
+/* This program is licensed under the terms of the GPL V3 or newer*/
+/* Written by Johannes Putzke*/
+/* eMail: die_eule@gmx.net*/ 
+
+
+public class Control_RunExternProgram implements Runnable  {
+	private String path;
+	
+	public Control_RunExternProgram(String path) {
+		this.path = path;
+	}
+	
+	public void run() {
+		try {
+			Runtime.getRuntime().exec(path);
+		} catch (Exception e) {
+		    System.err.println("Fehler beim Ausführen von"+path+e);
+		}
+	}
+}
