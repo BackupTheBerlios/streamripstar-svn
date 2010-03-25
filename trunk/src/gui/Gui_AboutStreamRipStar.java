99c40046845de602a19d53c2d2aac4069fe1dc44
eule <eule@163ef220-f67a-0410-9be0-c85765f07cbc>
2010-03-25 22:08:10 +0000

    with this 0.6 Version starts developing. Update bugfixes

--- trunk/src/gui/Gui_AboutStreamRipStar.java
+++ trunk/src/gui/Gui_AboutStreamRipStar.java
@@ -42,7 +42,7 @@ public class Gui_AboutStreamRipStar extends JFrame
 	private JLabel license = new JLabel("License :");
 	private JTextField licenceName = new JTextField("GPLV3");
 	private JLabel version = new JLabel("Version :");
-	private JTextField versionName = new JTextField("StreamRipStar 0.5.6");
+	private JTextField versionName = new JTextField("StreamRipStar 0.6.0-ALPHA1");
 	private JLabel revision = new JLabel("Revision :");
 	private JTextField revisionnumber = new JTextField("626");
 	private JLabel streamRipStarSourceforgeSite = new JLabel("StreamRipStars Projektpage :");
